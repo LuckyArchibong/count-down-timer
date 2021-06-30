b68c60b9700517cb3aa0d8c255a26ad0f7c0e8c0
Decrypt01 <lalucky22@gmail.com>
2021-06-30 22:09:35 +0000

    init

--- app.js
+++ app.js
@@ -21,3 +21,21 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+// assigning variables
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const property = document.querySelectorAll("deadline-FormData, h4");
+
+// setting up a date
+      // **set year**
+
+      const year = futureDate.getFullYear();
+      const hours = futureDate.getHours();
+      const minutes = futureDate.getMinutes();
+      
+      let month = futureDate.getMonth();
+      month = months[month];
+      const weekday = weekdays[futureDate.getDay()];
+      const date = futureDate.getDate();
+      giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
